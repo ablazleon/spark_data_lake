6162a19da1211496da1b5d9974f5f78c44b53a72
ablazleon <ablazleon@gmail.com>
2020-07-06 14:27:04 +0000

    aws header in dl.cfg

--- etl.py
+++ etl.py
@@ -10,8 +10,8 @@ config = configparser.ConfigParser()
 config.read('dl.cfg')
 print(config)
 
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
